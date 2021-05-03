3c3c135a3df3a6e3ab9807a89b3f2b40558cefed
Marco Tocchetto <madt88@gmail.com>
2021-05-03 14:00:05 -0300

    restart remote tunnel service when connection is down

--- connectivity.sh
+++ connectivity.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-LOG_FILE="/$HOME/.connection.log"
+LOG_FILE="$HOME/.connection.log"
 if ifconfig -a wwan0 | grep "inet "> /dev/null;then
 	echo $(date) OK >> $LOG_FILE
 else
 	echo $(date) NOT CONNECTED >> $LOG_FILE
 	echo "trying to reconnect..." >> $LOG_FILE	
 	systemctl restart simcom_wwan@wwan0.service
+	systemctl restart remote-tunnel.service 
 fi
