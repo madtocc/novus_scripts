8e9415bcdd3d1dd0af8b56dd74d81b5be66fb60e
Marco Tocchetto <madt88@gmail.com>
2021-06-23 09:58:47 -0300

    Update connectivity_install.sh

--- connectivity_install.sh
+++ connectivity_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/novus:${PWD}
-*/1 * * * * root /home/novus/connectivity.sh > /dev/null 2>&1" > /etc/cron.d/connection
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/novus:/home/novus/novus_scripts
+*/1 * * * * root /home/novus/novus_scripts/connectivity.sh > /dev/null 2>&1" > /etc/cron.d/connection
