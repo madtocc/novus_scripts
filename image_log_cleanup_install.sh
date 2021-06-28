53ef1e9b74c345050ce4d8d6a7cb5a4e67376689
Marco Tocchetto <madt88@gmail.com>
2021-06-28 12:30:41 -0300

    Update image_log_cleanup_install.sh

--- image_log_cleanup_install.sh
+++ image_log_cleanup_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/novus:${PWD}
-0 0 * * 0 root ${PWD}/image_log_cleanup.sh > /dev/null 2>&1" > /etc/cron.d/imglogcleanup
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/novus:/home/novus/novus_scripts
+0 0 * * 0 root /home/novus/novus_scripts/image_log_cleanup.sh > /dev/null 2>&1" > /etc/cron.d/imglogcleanup
